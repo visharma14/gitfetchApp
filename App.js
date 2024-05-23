e343432b7a59430ae0bd495b517f8908eb70ea1e
Vishal Sharma <visharma4972@gmail.com>
2024-05-23 12:56:48 +0530

    gitfetch

--- App.js
+++ App.js
@@ -6,6 +6,11 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text>
+          Hello World
+        </Text>
+      </View>
     </View>
   );
 }
@@ -18,3 +23,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
