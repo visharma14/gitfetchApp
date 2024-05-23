60770a4486e153d68f620fbf478a1f50a37d0e9f
Vishal Sharma <visharma4972@gmail.com>
2024-05-23 13:02:44 +0530

    gitfetch

--- App.js
+++ App.js
@@ -7,9 +7,8 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <View>
-        <Text>
-          Hello World
-        </Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
       </View>
     </View>
   );
